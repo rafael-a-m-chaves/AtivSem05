0851664f58981340f5dd8fafc3f3fa73e6045a66
rafael chaves <rafaelaugustochaves@hotmail.com>
2022-03-20 10:37:46 -0300

    controller e views

---
+++ LanchesMac/Controllers/PedidoController.cs
@@ -0,0 +1,74 @@
+﻿using LanchesMac.Models;
+using LanchesMac.Repositories.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace LanchesMac.Controllers
+{
+    public class PedidoController : Controller
+    {
+        private readonly IPedidoRepository _pedidoRepository;
+        private readonly CarrinhoCompra _carrinhoCompra;
+
+        public PedidoController(IPedidoRepository pedidoRepository, CarrinhoCompra carrinhoCompra)
+        {
+            _pedidoRepository = pedidoRepository;
+            _carrinhoCompra = carrinhoCompra;
+        }
+        [Authorize]
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Authorize]
+        public IActionResult Checkout(Pedido pedido)
+        {
+            decimal precoTotalPedido = 0.0m;
+            int totalItensPedido = 0;
+
+            List<CarrinhoCompraItem> items = _carrinhoCompra.GetCarrinhoCompraItens();
+
+            _carrinhoCompra.CarrinhoCompraItens = items;
+
+            if (_carrinhoCompra.CarrinhoCompraItens.Count == 0)
+            {
+                ModelState.AddModelError("", "Seu carrinho esta vazio, que tal incluir um lanche...");
+            }
+
+            foreach (var item in items)
+            {
+                totalItensPedido += item.Quantidade;
+                precoTotalPedido += (item.Lanche.Preco * item.Quantidade);
+            }
+
+            pedido.TotalItensPedido = totalItensPedido;
+
+            pedido.PedidoTotal = precoTotalPedido;
+
+            if (ModelState.IsValid)
+            {
+                _pedidoRepository.CriarPedido(pedido);
+
+                ViewBag.CheckoutCompletoMensagem = "Obrigado pelo seu pedido :) ";
+                ViewBag.TotalPedido = _carrinhoCompra.GetCarrinhoCompraTotal();
+
+                _carrinhoCompra.LimparCarrinho();
+                return View("~/Views/Pedido/CheckoutCompleto.cshtml", pedido);
+            }
+            return View(pedido);
+        }
+
+        public IActionResult CheckoutCompleto()
+        {
+            ViewBag.Cliente = TempData["Cliente"];
+            ViewBag.DataPedido = TempData["DataPedido"];
+            ViewBag.NumeroPedido = TempData["NumeroPedido"];
+            ViewBag.TotalPedido = TempData["TotalPedido"];
+            ViewBag.CheckoutCompletoMensagem = "Obrigado pelo seu pedido :) ";
+            return View();
+        }
+    }
+}
