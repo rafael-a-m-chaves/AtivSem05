06acb4b1660413802cc0af3b8fb0427479c8a4c1
rafael chaves <rafaelaugustochaves@hotmail.com>
2022-03-20 11:04:44 -0300

    context mapeado

--- LanchesMac/Context/AppDbContext.cs
+++ LanchesMac/Context/AppDbContext.cs
@@ -9,6 +9,12 @@ namespace LanchesMac.Context
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         { }
-        
+
+
+        public DbSet<Lanche> Lanches { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<CarrinhoCompraItem> CarrinhoCompraItens { get; set; }
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<PedidoDetalhe> PedidoDetalhes { get; set; }
     }
 }
